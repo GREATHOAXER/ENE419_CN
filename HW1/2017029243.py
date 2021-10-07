7be7013de19b2eb64ee6f4e0c7c0d68d6d59662c
한형서 <2017029243@hanyang.ac.kr>
2021-10-07 23:36:25 +0900

    2021.10.7 Conntected Telegram bot & Keyboard Interrupt Handling

--- HW1/2017029243.py
+++ HW1/2017029243.py
@@ -2,15 +2,24 @@ import requests
 import time
 import csv
 from time import gmtime, strftime
+from telegram.ext import Updater
 from apscheduler.schedulers.background import BackgroundScheduler
+#Global variables
+updater = Updater("2007419725:AAGmCJtaPECVdBwK8QjZSv3I2ALT66EfJbY")
 
 def test():
     r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,KRW")
     line = r.json()
     f = open('test.csv','a',newline="\n")
     wf = csv.writer(f)
-    wf.writerow([strftime("%Y-%m-%d-%H:%M:%S", time.localtime()),line["USD"],line["KRW"]])
+    coinList = [strftime("%Y-%m-%d-%H:%M:%S", time.localtime()),line["USD"],line["KRW"]]
+    wf.writerow(coinList)
     f.close()
+    try:
+        updater.bot.send_message(chat_id=2037124410, text=coinList)
+    except KeyboardInterrupt:
+        updater.bot.send_message(chat_id=2037124410, text='Keyboard Interrupt!')
+        exit(0)
 
     
 
@@ -27,4 +36,8 @@ if __name__ == '__main__':
     sched.add_job(test,'interval',seconds = 5,id="test")
     
     while True:
-        time.sleep(1.5)
+        try:
+            pass
+        except KeyboardInterrupt:
+            updater.bot.send_message(chat_id=2037124410, text='Keyboard Interrupt!')
+            exit(0)
