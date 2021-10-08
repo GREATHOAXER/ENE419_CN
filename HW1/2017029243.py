14bbb73be248c56b6ab806b4f5455227afe75417
한형서 <2017029243@hanyang.ac.kr>
2021-10-08 15:52:50 +0900

    2021.10.08 Get commands from user in telegram & Send to user the information about BitCoin

--- HW1/2017029243.py
+++ HW1/2017029243.py
@@ -2,42 +2,81 @@ import requests
 import time
 import csv
 from time import gmtime, strftime
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
+from telegram import Update, Bot
 from apscheduler.schedulers.background import BackgroundScheduler
-#Global variables
-updater = Updater("2007419725:AAGmCJtaPECVdBwK8QjZSv3I2ALT66EfJbY")
-
+    
 def test():
-    r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,KRW")
+    global previousPrice
+    concurrency : str = "USD"
+    concurrency2 : str = "KRW"
+    coin: str = "BTC"
+    r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+coin+"&tsyms="+concurrency+","+concurrency2)
     line = r.json()
-    f = open('test.csv','a',newline="\n")
-    wf = csv.writer(f)
-    coinList = [strftime("%Y-%m-%d-%H:%M:%S", time.localtime()),line["USD"],line["KRW"]]
-    wf.writerow(coinList)
-    f.close()
-    try:
-        updater.bot.send_message(chat_id=2037124410, text=coinList)
-    except KeyboardInterrupt:
-        updater.bot.send_message(chat_id=2037124410, text='Keyboard Interrupt!')
-        exit(0)
+    coinPriceInfo = [strftime("%Y-%m-%d-%H:%M:%S", time.localtime()),line[concurrency],line[concurrency2]]
+    textLine : str = ""
 
+    #File Part
+    f = open('coinInfo.csv','a',newline="\n") #Append coin information in existing file
+    wf = csv.writer(f)
+    wf.writerow(coinPriceInfo)
+    f.close()
+
+    #price selection
+    coinPrice = round(coinPriceInfo[1] - previousPrice, 3)
+    if(coinPrice > 0 and previousPrice != 0):
+        textLine: str = "Cryptocompare-"+coin+"-"+concurrency+": "+str(coinPriceInfo[1])+", UP " + str(coinPrice)
+        
+    elif(coinPrice < 0 and previousPrice != 0):
+        textLine: str = "Cryptocompare-"+coin+"-"+concurrency+": "+str(coinPriceInfo[1])+", DOWN " + str((abs(coinPrice)))
+
+    else:
+        textLine: str = "Cryptocompare-"+coin+"-"+concurrency+": "+str(coinPriceInfo[1])
+
+    #Send msg to telegram & update previous Price
+    updater.bot.send_message(chat_id = chatId,text = textLine)
+    previousPrice = coinPriceInfo[1]
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="작업을 시작합니다.")
+    sched.add_job(test,'cron',second = "00",id="test")
+    previousPrice = 0
+    print("작업 시작")
     
 
+def stop(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="작업을 끝냅니다.")
+    sched.remove_job('test')
+    print("작업 끝")
 
+#previous price initialization
+previousPrice = 0
 
-if __name__ == '__main__':
-    sched = BackgroundScheduler()
-    sched.start()
-    f = open('test.csv','w')
-    wf = csv.writer(f)
-    wf.writerow(["Date", "BTC-USD", "BTC-KRW"])
-    f.close()
-    #execute test every 5 seconds
-    sched.add_job(test,'interval',seconds = 5,id="test")
+#Scheduler settings
+sched = BackgroundScheduler()
+sched.start()
+
+#CSV file creation
+f = open('coinInfo.csv','w')
+fw = csv.writer(f)
+fw.writerow(["Date", "BTC-USD", "BTC-KRW"])
+f.close()
+
+botToken = "2007419725:AAGmCJtaPECVdBwK8QjZSv3I2ALT66EfJbY"
+chatId = 2037124410
+updater = Updater(botToken, use_context= True)
+
+dispatcher = updater.dispatcher
+start_handler = CommandHandler('start',start)
+end_handler = CommandHandler('stop',stop)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(end_handler)
     
-    while True:
-        try:
-            pass
-        except KeyboardInterrupt:
-            updater.bot.send_message(chat_id=2037124410, text='Keyboard Interrupt!')
-            exit(0)
+updater.start_polling()
+updater.idle()
+
+
+
+
+
